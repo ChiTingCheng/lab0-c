5163ebd3b8e84348268129e3dad5c44cb1d39869
Cheng_Chi_Ting <mojangtimmy@gmail.com>
2024-03-01 10:10:18 +0800

    Complete q_insert_tail() function
    Fill the lost parts in the function 'q_insert_tail'.

--- queue.c
+++ queue.c
@@ -45,7 +45,20 @@ bool q_insert_head(struct list_head *head, char *s)
 /* Insert an element at tail of queue */
 bool q_insert_tail(struct list_head *head, char *s)
 {
+    if (!head || !s)
+        return false;
+    element_t *node = (element_t *) malloc(sizeof(element_t));
+    if (!node)
+        return false;
+    node->value = strdup(s);
+    if (!node->value) {
+        free(node);
+        return false;
+    }
+    INIT_LIST_HEAD(&(node->list));
+    list_add_tail(&node->list, head);
     return true;
+
 }
 
 /* Remove an element from head of queue */
